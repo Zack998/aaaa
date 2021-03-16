c8ff3210b57db0e16ec9104c8d388a90d8011917
Zack998 <78541864+Zack998@users.noreply.github.com>
2021-03-15 20:22:24 -0500

    Add files via upload

---
+++ 打印星号.java
@@ -0,0 +1,12 @@
+package zk1;
+
+public class ¥Ú”°–«∫≈ {
+	public static void main(String[] args) {
+		System.out.println("*");
+		System.out.println("* *");
+		System.out.println("* * *");
+		System.out.println("* * * *");
+		System.out.println("* * * * *");
+	}
+
+}
